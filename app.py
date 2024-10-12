39eb798f38b3058908aee8282f8c1d2041a5b108
marto012 <167631714+marto012@users.noreply.github.com>
2024-10-12 13:30:35 +0200

    vaja z želvo do drugega primera

---
+++ app.py
@@ -0,0 +1,34 @@
+# RISANJE Z ŽELVO-ŽELVJA GRAFIKA
+turtle.done()
+import turtle
+martin = turtle.Pen()
+martin.reset()
+'''martin.forward(100)'''
+# 1. primer
+'''martin = turtle.Pen()
+martin.forward(100)
+martin.left(90)
+martin.forward(50)
+martin.left(90)
+martin.forward(100)
+martin.left(90)
+martin.forward(50)
+martin.left(90)'''
+# 2. primer
+'''martin.color('red')
+martin.forward(50)
+martin.up()
+martin.forward(30)
+martin.down()
+martin.color('green')
+martin.forward(50)
+martin.up()
+martin.forward(30)
+martin.down()
+martin.color('yellow')
+martin.forward(50)
+martin.up()
+martin.forward(30)
+martin.down()'''
+# 3. primer
+
