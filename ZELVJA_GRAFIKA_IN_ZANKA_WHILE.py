b98c2f6cf9550d7a2f0ac47618c0558e9b9be2fa
marto012 <167631714+marto012@users.noreply.github.com>
2024-10-13 10:36:48 +0200

    novo poglavje 2

--- ZELVJA_GRAFIKA_IN_ZANKA_WHILE.py
+++ ZELVJA_GRAFIKA_IN_ZANKA_WHILE.py
@@ -1,15 +1,26 @@
 # ŽELVJA GRAFIKA IN ZANKA WHILE
 from kvadrat_z_zanko_while import kvadrat_z_zanko_while
+from devetkotni_in_krog import devetkotnik
+from devetkotni_in_krog import krog
 
 
 def main():
     print("Izberi program, ki ga želiš izvesti:")
-    print("1. vagon")
-    
-    choice = input("Vnesi število programa (1-1): ")
+    print("1. kvadrat z zanko while")
+    print('2. devetkotnik ')
+    print('3. krog')
+
+    choice = input("Vnesi število programa (1-3): ")
 
     if choice == "1":
         kvadrat_z_zanko_while()
+    
+    elif choice == '2':
+        devetkotnik()
+    
+    elif choice == '3':
+        krog()
+
     else:
         print("Neveljavna izbira!")
 
---
+++ devetkotni_in_krog.py
@@ -0,0 +1,27 @@
+import turtle
+
+
+def devetkotnik():
+    martin = turtle.Pen()
+    martin.reset()
+    i = 1
+
+    while i <= 9:
+        martin.forward(50)
+        martin.left(360 / 9)
+        i = i + 1
+
+    turtle.done()
+
+
+def krog():
+    martin = turtle.Pen()
+    martin.reset()
+    i = 1
+
+    while i <= 120:
+        martin.forward(3)
+        martin.left(360 / 120)
+        i = i + 1
+
+    turtle.done()
